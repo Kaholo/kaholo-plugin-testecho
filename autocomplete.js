834af8cadcc49b5481bed1418d1280c242570b34
Kenny Marsh <kenny.m@kaholo.io>
2023-04-22 09:23:14 +0200

    version bump

--- autocomplete.js
+++ autocomplete.js
@@ -1,14 +1,13 @@
 async function pickLetter() {
-
   const letters = ["alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india"];
   const mappedAutocompleteItems = letters.map((letter) => ({
     id: `${letter.charAt(0).toUpperCase()}`,
     value: letter,
   }));
 
-    return mappedAutocompleteItems;
+  return mappedAutocompleteItems;
 }
 
 module.exports = {
-    pickLetter,
+  pickLetter,
 };
